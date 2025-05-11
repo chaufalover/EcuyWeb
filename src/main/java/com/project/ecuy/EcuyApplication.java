75d50629c13f252dfe7ffc7cfa71ade8005abc29
chaufalover <tacuresancheztomas@gmail.com>
2025-05-10 22:50:37 -0500

    Inicio del proyecto

---
+++ src/main/java/com/project/ecuy/EcuyApplication.java
@@ -0,0 +1,13 @@
+package com.project.ecuy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcuyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcuyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/ecuy/EcuyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.ecuy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcuyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
